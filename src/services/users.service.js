c0b5738565f4d69fa9c8096a27d23d4719d9b90e
Rubens Vitor de Barros Almeida <rubens.almeida@localiza.com>
2022-04-01 15:52:11 -0300

    adjust current page

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,26 +1,27 @@
 const request = require('request');
-var url = require('url') ;
+var url = require('url');
 
 const header = { 'user-agent': 'node.js' };
 
-const getUsers = function(since, req, res) {
+const getUsers = function (since, req, res) {
     request('https://api.github.com/users?since=' + since, { method: 'GET', headers: header }, (error, response, body) => {
-    since++;
-    const baseUrl = 'https://' + req.headers.host;
-    res.send({
+        const next = since + 1;
+        const baseUrl = 'https://' + req.headers.host;
+        res.send({
             body: JSON.parse(response.body),
-            next: baseUrl + '/users?since=' + since
+            current: since,
+            next: baseUrl + '/users?since=' + next
         });
     });
 }
 
-const getUserDetails = function(username, res) {
+const getUserDetails = function (username, res) {
     request(`https://api.github.com/users/${username}`, { method: 'GET', headers: header }, (error, response, body) => {
         res.send(JSON.parse(response.body));
     });
 }
 
-const getUserRepos = function(username, res) {
+const getUserRepos = function (username, res) {
     request(`https://api.github.com/users/${username}/repos`, { method: 'GET', headers: header }, (error, response, body) => {
         res.send(JSON.parse(response.body));
     });
